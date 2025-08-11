456c40627a88e2b6f54e667469e23224d7314c0e
Ahmed Elsayyad <ahmedelsayyad006@gmail.com>
2025-08-11 14:44:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -556,7 +556,7 @@ class NovaBootcamp {
           {
             title: "What is Design Thinking",
             url: "https://www.youtube.com/embed/tRHds9dmPuc?si=_fB8nWGfAjXN992U",
-            thumbnail: "https://img.youtube.com/vi/tRHds9dmPuc/maxresdefault.jpg",
+            thumbnail: "https://img.youtube.com/vi/tRHds9dmPuc/./assets/images/def.png",
             duration: "10:15",
             description: "Learn the basics of Design Thinking — a simple, creative approach to solving problems. We’ll guide you through its key stages and show how it can spark innovation in any field."
           },
@@ -1595,6 +1595,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   console.log('🚀 NOVA Business Bootcamp loaded successfully!');
 });
+
 
 
 
